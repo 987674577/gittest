fba5a02bc44f42041e334bd409715060977b74fd
liuxiaofeng <987674577@qq.com>
2018-10-14 19:46:26 +0800

    添加test工程到git

---
+++ gittest/src/main/java/com/itheima/main/Test.java
@@ -0,0 +1,10 @@
+package com.itheima.main;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("柯航傻逼");
+        System.out.println("柯航傻逼");
+        System.out.println("柯航傻逼");
+        System.out.println("柯航傻逼");
+    }
+}
